f737e8d46beaa8bbc616b00d9e68033a9b13f023
Terry Yang <terry.yang@kooppi.com>
2017-05-26 12:02:15 +0800

    Initial Commit

---
+++ NotUseStoryboardUITests/NotUseStoryboardUITests.swift
@@ -0,0 +1,36 @@
+//
+//  NotUseStoryboardUITests.swift
+//  NotUseStoryboardUITests
+//
+//  Created by Terry Yang on 2017/5/26.
+//  Copyright © 2017年 terryyamg. All rights reserved.
+//
+
+import XCTest
+@testable import NotUseStoryboardUI
+
+class NotUseStoryboardUITests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
